cf261df4e32422c707f2c0e91a16edc5c64f94b6
Aarons <1074195910@qq.com>
2021-08-16 02:08:50 +0800

    13 commit introduction done

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -1,5 +1,9 @@
-import { View } from '@tarojs/components'
+import { useState } from 'react'
+import { View, Text } from '@tarojs/components'
+import { AtIcon, AtTag } from 'taro-ui'
 import { logo } from '../../constances/imgAddress'
+import 'taro-ui/dist/style/components/icon.scss'
+import 'taro-ui/dist/style/components/tag.scss'
 import './index.less'
 
 function Header(upperBarHeight) {
@@ -15,13 +19,39 @@ function Header(upperBarHeight) {
     )
 }
 
-function Introduction(){
+function Introduction() {
+
+    const [ellipsis, setEllipsis] = useState(true)
+
+    //展开/收起note的回调
+    const openNote = () => {
+        setEllipsis(!ellipsis)
+    }
+
+    const note = '公告：欢迎光临本餐厅，很高兴为宁服务~菜品皆虚构，出自米哈游-原神，侵删谢谢~'
     return (
         <View className='introduction'>
-            <View className='title'></View>
-            <View className='comment'></View>
-            <View className='discount'></View>
-            <View className='note'></View>
+            <View className='title'>薯条餐厅</View>
+            <View className='comment'>
+                <Text className='comment1'>⭐ 4.9</Text>
+                <Text className='comment2'>月售999+</Text>
+            </View>
+            <View className='discount'>
+                <AtTag size='small'>满50-8</AtTag>
+                <AtTag size='small'>满80-20</AtTag>
+                <AtTag size='small'>满150-50</AtTag>
+            </View>
+            {ellipsis?(
+            <View className='note-ellipsis'>
+                <View children={note}/>
+                <AtIcon value='chevron-down' size='15' color='#888' onClick={openNote}/>
+            </View>
+            ):(
+            <View className='note-no-ellipsis'>
+                <View children={note}/>
+                <AtIcon value='chevron-up' size='15' color='#888' onClick={openNote}/>
+            </View>
+            )}
             <View className='logo'></View>
         </View>
     )
